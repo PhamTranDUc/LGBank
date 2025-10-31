6f3f7cab901da1ac62206557f1f50bd9fdc9a31a
phamduc <phamtranducc@gmail.com>
2025-10-31 17:06:20 +0700

    add webhook auto refresh config from github

--- cards/cards/src/main/java/com/LGBank/cards/dto/CardInforContactDto.java
+++ cards/cards/src/main/java/com/LGBank/cards/dto/CardInforContactDto.java
@@ -1,10 +1,17 @@
 package com.LGBank.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.HashMap;
 import java.util.List;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardInforContactDto(String message, HashMap<String, String> contactDetails, List<String> onCallSupport )  {
+@Getter
+@Setter
+public class CardInforContactDto  {
+    private String message;
+    private  HashMap<String, String> contactDetails;
+    private List<String> onCallSupport;
 }

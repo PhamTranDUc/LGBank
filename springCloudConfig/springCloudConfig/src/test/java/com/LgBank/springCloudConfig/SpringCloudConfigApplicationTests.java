27562490cc5f8f17d4cb907a7093a16cdca141f9
phamduc <phamtranducc@gmail.com>
2025-11-03 18:16:38 +0700

    add code config webhook auto refresh config from github

---
+++ springCloudConfig/springCloudConfig/src/test/java/com/LgBank/springCloudConfig/SpringCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LgBank.springCloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

23fae2560a44a69ab75843f89397085e8c178c45
phamduc <phamtranducc@gmail.com>
2026-01-12 17:07:53 +0700

    fig error for run dockerfile

--- springcloudconfig/springcloudconfig/src/main/java/com/LgBank/springCloudConfig/SpringCloudConfigApplication.java
+++ springcloudconfig/springcloudconfig/src/main/java/com/LgBank/springCloudConfig/SpringCloudConfigApplication.java
@@ -1,4 +1,4 @@
-package com.LgBank.springCloudConfig;
+package com.LGBank.springCloudConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
